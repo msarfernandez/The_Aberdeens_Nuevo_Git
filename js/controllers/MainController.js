9c4f771cbe4e72a923564f35c19ddc22d79b0eb8
msarfernandez <msarfernandez@gmail.com>
2016-07-04 13:22:25 -0300

    c

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,5 +1,24 @@
 app.controller('MainController', ['$scope', function($scope){
 
+
+	//aca tendria que poner variables para las fechas
+	var	day = new Date("February 28, 2016").getDay();
+	var week = new Array('Dom', 'Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sab');
+	//week[day]
+	var fecha = new Date("February 28, 2016");
+
+	$scope.prueba = week[new Date("February 28, 2016").getDay()];
+
+	// aca tendria que poner variables para la fecha
+		//document.write(new Date("February 28, 2016").getDate());
+
+		$scope.dia = {
+			fecha: fecha.getDate(),
+			dia: week[day]
+		};
+
+
+
 	$scope.eventos =
 	[
 	{
---
+++ js/directives/fechaFigure.js
@@ -0,0 +1,9 @@
+app.directive('fechaFigure',function() {
+	return {
+    	restrict: 'E',
+    	scope: {
+    		p: '='
+    	},
+    	templateUrl: 'js/directives/fechaFigure.html'
+  	};
+});
